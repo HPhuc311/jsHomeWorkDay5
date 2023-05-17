39db1ebe4a13ac5ee3c9f57613433611a53c6b28
HoangPhuc <hoangphuc311tn@gmail.com>
2023-05-17 16:19:44 +0700

    done

--- JS/EXJS3.js
+++ JS/EXJS3.js
@@ -6,20 +6,21 @@ function taxofIncome(){
     if(tax <= 60e+6){
         income = tax * 0.05
     }else if(tax > 60e+6 && tax <= 120e+6){
-        income = tax * 0.05 + (tax - 60e+6) * 0.05;
+        income = tax * 0.05 + (tax - 60e+6) * 0.05;     
     }else if(tax > 120e+6 && tax <= 210e+6){
-        income = tax * 0.05 + (tax - 60e+6) * 0.1 + (tax - 120e+6) * 0.15;
+        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.05;
     }else if(tax > 210 && tax <= 384){
-        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.1;
+        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.05 + (tax - 210e+6) * 0.05;
     }else if(tax > 384 && tax <= 624){
-        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.1 + (tax - 384e+6) * 0.15;
+        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.05 + (tax - 384e+6) * 0.05;
     }else if(tax > 624 && tax <= 960){
-        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.1 + (tax - 384e+6) * 0.15 + (tax - 624e+6) * 0.15;
+        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.05 + (tax - 384e+6) * 0.05 + (tax - 624e+6) * 0.05;
     }else{
-        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.1 + (tax - 384e+6) * 0.15 + (tax - 624e+6) * 0.15 + (tax - 960e+6) * 0.2;
+        income = tax * 0.05 + (tax - 60e+6) * 0.05 + (tax - 120e+6) * 0.1 + (tax - 210e+6) * 0.1 + (tax - 384e+6) * 0.05 + (tax - 624e+6) * 0.05 + (tax - 960e+6) * 0.05;
     }
 
-    document.getElementById('results2').innerHTML = income
+    document.getElementById('results1').innerHTML = "Tên khách hàng:" + name
+    document.getElementById('results2').innerHTML = "Thuế thu nhập cá nhân của bạn là:" + new Intl.NumberFormat('vn-VN').format(income) + "VND"
 }
 
 document.getElementById("btnofResults").onclick = taxofIncome;
